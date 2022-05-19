e18e615a89ffe48a1ed27967dda52d72e1e4de8b
Sudhir Chavhan <sudhirchavhan100@gmail.com>
2022-05-19 20:05:30 +0530

    done
    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Structure } from './Components/Structure.jsx';
+
+function App() {
+
+  return (
+    <div >
+      { <Structure/> }
+ 
+    </div>
+  );
+}
+
+export default App;
